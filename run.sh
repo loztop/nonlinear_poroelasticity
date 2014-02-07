2a27b46b6e92f0ed50e591ac7a57e75d1b433b83
Lorenz Berger <lorenzb@compute-lung.cs.ox.ac.uk>
2014-02-07 14:16:35 +0000

    firts comit, with weak bcs and traction linearization working

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#set -x
+
+source $LIBMESH_DIR/examples/run_common.sh
+
+#example_name=systems_of_equations_ex4
+example_name=nonlinear_poroelasticity
+
+options="-ksp_type preonly -pc_factor_mat_solver_package mumps -pc_type lu"
+
+#options="-ksp_view"
+
+run_example "$example_name" "$options"
---
+++ systems_of_equations_ex4.C
@@ -0,0 +1,58 @@
+/*
+
+// C++ include files that we need
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+
+// libMesh includes
+#include "libmesh/libmesh.h"
+#include "libmesh/mesh.h"
+#include "libmesh/mesh_generation.h"
+#include "libmesh/exodusII_io.h"
+#include "libmesh/gnuplot_io.h"
+#include "libmesh/linear_implicit_system.h"
+#include "libmesh/equation_systems.h"
+#include "libmesh/fe.h"
+#include "libmesh/quadrature_gauss.h"
+#include "libmesh/dof_map.h"
+#include "libmesh/sparse_matrix.h"
+#include "libmesh/numeric_vector.h"
+#include "libmesh/dense_matrix.h"
+#include "libmesh/dense_submatrix.h"
+#include "libmesh/dense_vector.h"
+#include "libmesh/dense_subvector.h"
+#include "libmesh/perf_log.h"
+#include "libmesh/elem.h"
+#include "libmesh/boundary_info.h"
+#include "libmesh/zero_function.h"
+#include "libmesh/dirichlet_boundaries.h"
+#include "libmesh/string_to_enum.h"
+#include "libmesh/getpot.h"
+
+
+#include "libmesh/petsc_linear_solver.h"
+#include "libmesh/petsc_matrix.h"
+
+
+
+#define SOLVER_NAME "mumps"
+
+
+
+
+#define PC_TYPE PCLU
+#define PETSC_MUMPS 1
+
+
+
+// Bring in everything from the libMesh namespace
+using namespace libMesh;
+
+
+
+
+void test(int a) {
+std::cout << "ex "<< a << std::endl;
+}
+*/
